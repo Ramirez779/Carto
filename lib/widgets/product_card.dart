08847184f5929560cc68101d36e6f8a34b74335f
antonio77 <ramirezantonio7799@gmail.com>
2026-01-07 19:25:43 -0600

    se separa una parte de home_screen a product_card, basicamente para que este archivo no tenga mucha carga

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:carto/models/product.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback onTap;
+
+  const ProductCard({
+    super.key,
+    required this.product,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      label: 'Producto ${product.title}, precio ${product.price} dólares',
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 10,
+                offset: const Offset(0, 6),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Icon(
+                    Icons.shopping_bag_outlined,
+                    size: 42,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              Text(
+                product.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                '\$${product.price}',
+                style: const TextStyle(
+                  color: Colors.black54,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
